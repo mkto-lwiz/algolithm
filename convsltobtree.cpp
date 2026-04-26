80afe9f107b56d2bd011a24c4809a01c9f595dc7
Lew <lewiskwokzz@gmail.com>
2026-04-26 15:18:27 +0700

    with a simple test case

--- convsltobtree.cpp
+++ convsltobtree.cpp
@@ -55,11 +55,46 @@ class solulu
 private:
 
 public:
-    
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> turnintoarr;
+        ListNode* current = head;
+        while (current != nullptr)
+        {
+            int temp = current->val;
+            turnintoarr.push_back(temp);
+            current = current->next;
+        }
+        return builtree(turnintoarr,0,turnintoarr.size()-1);
+    }
+    TreeNode * builtree(vector<int>& turnintoarr, int left, int right){
+        if(left > right){
+            return nullptr;
+        }
+        //complete search
+        int mid = left + (right-left)/2;
+
+        TreeNode* leftree = builtree(turnintoarr, left, mid-1);
+        TreeNode* rightree = builtree(turnintoarr, mid+1,right);
+
+        TreeNode* root = new TreeNode(turnintoarr[mid], leftree, rightree);
+        return root;
+    }
 };
 
 
 
 int main(){
+    solulu s;
+    // Create a sorted linked list: 1->2->3->4->5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
 
+    TreeNode* root = s.sortedListToBST(head);
+    printree(root);
+    cout << endl;
+
+    return 0;
 }
